8175b59cb74fd1cf93359dba55c0d04e00a54ac3
ssd2658 <ssd2658@gmail.com>
2026-04-25 00:06:47 +0530

    feat: scaffold core SDKs, portfolio models, and automated CI/CD workflows for AM services

---
+++ libraries/am-core-lib/src/main/java/com/am/common/model/dto/EquityHoldingDto.java
@@ -0,0 +1,24 @@
+package com.am.common.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EquityHoldingDto {
+    private String isin;
+    private String symbol;
+    private Double quantity;
+    private Double currentPrice;
+    private Double currentValue;
+    private Double investmentValue;
+    private Double investmentCost;
+    private Double profitLoss;
+    private Double profitLossPercentage;
+    private Double todayProfitLoss;
+    private Double todayProfitLossPercentage;
+}
---
+++ libraries/am-core-lib/src/main/java/com/am/common/model/dto/PortfolioUpdateDto.java
@@ -0,0 +1,25 @@
+package com.am.common.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PortfolioUpdateDto {
+    private String userId;
+    private List<EquityHoldingDto> equities;
+
+    // Summary Fields
+    private Double currentValue;
+    private Double investmentValue;
+    private Double totalGainLoss;
+    private Double totalGainLossPercentage;
+    private Double todayGainLoss;
+    private Double todayGainLossPercentage;
+}
