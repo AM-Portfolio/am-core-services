d1a91a31c259039a155a2a7d86501a5810e55cc3
Sahim <m.sahimuzzaman@gmail.com>
2026-06-03 23:28:00 +0530

    fix: add @ConditionalOnMissingBean to prevent GlobalExceptionHandler bean conflict

--- libraries/am-api-core-lib/src/main/java/com/am/api/core/config/AmApiCoreAutoConfiguration.java
+++ libraries/am-api-core-lib/src/main/java/com/am/api/core/config/AmApiCoreAutoConfiguration.java
@@ -3,17 +3,20 @@ package com.am.api.core.config;
 import com.am.api.core.advice.GlobalExceptionHandler;
 import com.am.api.core.advice.GlobalResponseWrapper;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 public class AmApiCoreAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean(name = "globalExceptionHandler")
     public GlobalExceptionHandler globalExceptionHandler() {
         return new GlobalExceptionHandler();
     }
 
     @Bean
+    @ConditionalOnMissingBean(name = "globalResponseWrapper")
     public GlobalResponseWrapper globalResponseWrapper() {
         return new GlobalResponseWrapper();
     }
